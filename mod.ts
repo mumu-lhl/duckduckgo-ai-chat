fb2d492a5f2ed52119d959949ecb9cad0d69fe04
Mumulhl <mumulhl.666@gmail.com>
2025-02-08 14:03:34 +0800

    docs: update documentation

--- mod.ts
+++ mod.ts
@@ -1,11 +1,11 @@
 /**
- * A module provides the [Duckduckgo AI Chat](https://duckduckgo.com/aichat) API for JavaScript/TypeScript, which can use gpt-4o-mini for free.
+ * A module provides the [Duckduckgo AI Chat](https://duckduckgo.com/aichat) API for JavaScript/TypeScript, which can use o3-mini for free.
  * @example
  * ```ts
  * import { initChat } from "@mumulhl/duckduckgo-ai-chat";
  *
- * // Initialize, optional models are gpt-4o-mini, claude-3-haiku-20240307, meta-llama/Llama-3-70b-chat-hf, mistralai/Mixtral-8x7B-Instruct-v0.1
- * const chat = await initChat("gpt-4o-mini");
+ * // Initialize, optional models are gpt-4o-mini, claude-3-haiku, llama, mixtral, o3-mini
+ * const chat = await initChat("o3-mini");
  *
  * // Fetch the full reply in one go
  * let message = await chat.fetchFull("Hello");
